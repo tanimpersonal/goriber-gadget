ca8c58422336bd18858905948aece9049bdb4936
tanimpersonal <tashintanim2019@gmail.com>
2022-03-02 01:55:20 +0600

    spinner added

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,46 @@
+// variables for use
 const inputField = document.getElementById('input');
 const searchButton = document.getElementById('search');
 const cardSection = document.getElementById('card-section');
 const loadMore = document.getElementById('load-more');
 loadMore.style.display = 'none';
 const loadButton= document.getElementById('load-button');
+const spinner= document.getElementById('my-spinner');
+// variable end
+// search click
 searchButton.addEventListener('click', function (event) {
   const inputValue = inputField.value;
+  inputField.value='';
+  cardSection.textContent='';
+  detailSection.textContent='';
   fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
     .then(Response => Response.json())
     .then(data => display(data))
 })
+// search click end
+// display card
 const display = data => {
   const length= data.data.length;
   console.log(length);
-  if (data.data.length > 0) {
+  if (data.data.length > 0) {    
     if (data.data.length < 20) {
+      loadMore.style.display = 'none';
+      spinner.classList.add('spinner-border');
       for (const myData of data.data) {
         // console.log(myData);
         cardCreator(myData);
       }
+      spinner.classList.add('d-none');
     } else if (data.data.length > 20) {
+      spinner.classList.add('spinner-border');
+      loadButton.removeAttribute('disabled');
       loadMore.style.display = 'block';
+      spinner.style.display= 'flex';
       for(let i=0; i<20; i++){
         console.log(data.data[i]);
         cardCreator(data.data[i]);
       }
+      spinner.classList.add('d-none');
       loadButton.addEventListener('click',function(){
         for(let i=length-1; i>=20; i--){
           console.log(data.data[i]);
@@ -32,25 +48,37 @@ const display = data => {
         }
         loadButton.setAttribute('disabled',true);
       })
+      
     }
 
   } else {
+    spinner.classList.add('spinner-border');
+    spinner.classList.add('d-flex');
+    loadMore.style.display = 'none';
     cardSection.innerHTML = `
         <div class="not-found"><i class="fa-solid d-inline-block fa-exclamation"></i><i class="fa-solid d-inline-block fa-heart-crack"></i>
         <p>No Result Found</p></div>
         `
   }
+  spinner.classList.remove('d-flex');
+  spinner.classList.add('d-none');
 }
+// display card end
+// detail section
 const detailSection = document.getElementById('detail-section');
-
+// detail section end
+// function moredetail
 function moreDetail(id, image) {
   console.log(id);
   fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     .then(Response => Response.json())
     .then(data => displayMore(data, image))
 }
+// function more detail end
+// details section
 const displayMore = (data, image) => {
-  detailSection.innerHTML = `<div class="card h-100 custom-card" style="width: 18rem;">
+  detailSection.textContent='';
+  detailSection.innerHTML = `<div class="card h-100 w-50 custom-card">
     <img src="${image}" class="card-img-top" alt="...">
     <div class="card-body">
       <p id="release" class="card-text"></p>
@@ -77,7 +105,8 @@ const displayMore = (data, image) => {
     behavior: 'smooth'
   });
 }
-
+//details section end
+// card create
 const cardCreator = (myData) => {
   const div = document.createElement('div');
   div.innerHTML = `<div class="card h-100 custom-card">
@@ -91,3 +120,4 @@ const cardCreator = (myData) => {
   div.classList.add('col');
   cardSection.appendChild(div);
 }
+// card create end
