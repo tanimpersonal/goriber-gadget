cbfb7af6ae05f2cb21af9dac71a2acc0e82a721d
tanimpersonal <tashintanim2019@gmail.com>
2022-03-01 07:08:24 +0600

    sensor added

--- JS/app.js
+++ JS/app.js
@@ -17,7 +17,7 @@ const display= data=>{
             <div class="card-body">
               <h5 class="card-title">${myData.phone_name}</h5>
               <p class="card-text">Brand Name: ${myData.brand}</p>
-              <button class="btn btn-primary" onclick="moreDetail('${myData.slug}')">Explore More!</button>
+              <button class="btn btn-primary" onclick="moreDetail('${myData.slug}','${myData.image}')">Explore More!</button>
             </div>
           </div>`
             div.classList.add('col');
@@ -31,9 +31,33 @@ const display= data=>{
     }
 }
 const detailSection= document.getElementById('detail-section');
-function moreDetail(id){
+function moreDetail(id,image){
     console.log(id);
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     .then(Response=>Response.json())
-    .then(data=>displayMore(data))
+    .then(data=>displayMore(data,image))
+}
+const displayMore= (data,image) =>{
+    detailSection.innerHTML=`<div class="card h-100 custom-card" style="width: 18rem;">
+    <img src="${image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p id="release" class="card-text"></p>
+      <p id="sensor"></p>
+    </div>
+  </div>`
+  const release=document.getElementById('release');
+    if(data.data.releaseDate!=''){
+        release.innerText=data.data.releaseDate;
+    }else{
+        release.innerText='Release Date: Not Found';
+    }
+    const span= document.getElementById('sensor');
+    console.log(data.data.mainFeatures.sensors);
+    if(data.data.mainFeatures.sensors!=''){
+        let total='';
+        for(const sensor of data.data.mainFeatures.sensors){
+            total=total+sensor+',';
+        }
+        span.innerText= 'Sensors: '+total;
+    }
 }
