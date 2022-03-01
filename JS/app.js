f525559d5746440932a9424f999377fffd645b36
tanimpersonal <tashintanim2019@gmail.com>
2022-03-01 20:13:31 +0600

    load more 20+ works

--- JS/app.js
+++ JS/app.js
@@ -1,63 +1,93 @@
-const inputField= document.getElementById('input');
-const searchButton= document.getElementById('search');
-const cardSection= document.getElementById('card-section');
-searchButton.addEventListener('click',function(event){
-    const inputValue= inputField.value;
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
-    .then(Response=>Response.json())
-    .then(data=>display(data))
+const inputField = document.getElementById('input');
+const searchButton = document.getElementById('search');
+const cardSection = document.getElementById('card-section');
+const loadMore = document.getElementById('load-more');
+loadMore.style.display = 'none';
+const loadButton= document.getElementById('load-button');
+searchButton.addEventListener('click', function (event) {
+  const inputValue = inputField.value;
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+    .then(Response => Response.json())
+    .then(data => display(data))
 })
-const display= data=>{
-    if(data.data.length>0){
-        for(const myData of data.data){
-            const num1=8;
-            const div= document.createElement('div');
-            div.innerHTML=`<div class="card h-100 custom-card">
-            <img src="${myData.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${myData.phone_name}</h5>
-              <p class="card-text">Brand Name: ${myData.brand}</p>
-              <button class="btn btn-primary" onclick="moreDetail('${myData.slug}','${myData.image}')">Explore More!</button>
-            </div>
-          </div>`
-            div.classList.add('col');
-            cardSection.appendChild(div);
+const display = data => {
+  const length= data.data.length;
+  console.log(length);
+  if (data.data.length > 0) {
+    if (data.data.length < 20) {
+      for (const myData of data.data) {
+        // console.log(myData);
+        cardCreator(myData);
+      }
+    } else if (data.data.length > 20) {
+      loadMore.style.display = 'block';
+      for(let i=0; i<20; i++){
+        console.log(data.data[i]);
+        cardCreator(data.data[i]);
+      }
+      loadButton.addEventListener('click',function(){
+        for(let i=length-1; i>=20; i--){
+          console.log(data.data[i]);
+          cardCreator(data.data[i]);
         }
-    } else{
-        cardSection.innerHTML= `
+        loadButton.setAttribute('disabled',true);
+      })
+    }
+
+  } else {
+    cardSection.innerHTML = `
         <div class="not-found"><i class="fa-solid d-inline-block fa-exclamation"></i><i class="fa-solid d-inline-block fa-heart-crack"></i>
         <p>No Result Found</p></div>
         `
-    }
+  }
 }
-const detailSection= document.getElementById('detail-section');
-function moreDetail(id,image){
-    console.log(id);
-    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-    .then(Response=>Response.json())
-    .then(data=>displayMore(data,image))
+const detailSection = document.getElementById('detail-section');
+
+function moreDetail(id, image) {
+  console.log(id);
+  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(Response => Response.json())
+    .then(data => displayMore(data, image))
 }
-const displayMore= (data,image) =>{
-    detailSection.innerHTML=`<div class="card h-100 custom-card" style="width: 18rem;">
+const displayMore = (data, image) => {
+  detailSection.innerHTML = `<div class="card h-100 custom-card" style="width: 18rem;">
     <img src="${image}" class="card-img-top" alt="...">
     <div class="card-body">
       <p id="release" class="card-text"></p>
       <p id="sensor"></p>
     </div>
   </div>`
-  const release=document.getElementById('release');
-    if(data.data.releaseDate!=''){
-        release.innerText=data.data.releaseDate;
-    }else{
-        release.innerText='Release Date: Not Found';
+  const release = document.getElementById('release');
+  if (data.data.releaseDate != '') {
+    release.innerText = data.data.releaseDate;
+  } else {
+    release.innerText = 'Release Date: Not Found';
+  }
+  const span = document.getElementById('sensor');
+  console.log(data.data.mainFeatures.sensors);
+  if (data.data.mainFeatures.sensors != '') {
+    let total = '';
+    for (const sensor of data.data.mainFeatures.sensors) {
+      total = total + sensor + ',';
     }
-    const span= document.getElementById('sensor');
-    console.log(data.data.mainFeatures.sensors);
-    if(data.data.mainFeatures.sensors!=''){
-        let total='';
-        for(const sensor of data.data.mainFeatures.sensors){
-            total=total+sensor+',';
-        }
-        span.innerText= 'Sensors: '+total;
-    }
+    span.innerText = 'Sensors: ' + total;
+  }
+  window.scroll({
+    top: 60,
+    behavior: 'smooth'
+  });
+}
+
+const cardCreator = (myData) => {
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="card h-100 custom-card">
+      <img src="${myData.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${myData.phone_name}</h5>
+        <p class="card-text">Brand Name: ${myData.brand}</p>
+        <button class="btn btn-primary" onclick="moreDetail('${myData.slug}','${myData.image}')">Explore More!</button>
+      </div>
+    </div>`
+  div.classList.add('col');
+  cardSection.appendChild(div);
 }
